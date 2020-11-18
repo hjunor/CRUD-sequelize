8801778d387e04c14408c9176a240e4bbfa69d19
Heberth Junor <hjunor@gmail.com>
2020-11-18 15:19:12 -0300

    Add: Sequelize configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const db = require("./models/index");
+
 const authController = require("./controllers/auth");
 const app = express();
 
 app.use("/auth", authController);
 
-app.listen(3001, () => {
-  console.log("Server start 🔥");
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server start 🔥");
+  });
 });
