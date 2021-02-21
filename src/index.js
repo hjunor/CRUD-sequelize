60ca114214356c247fbc22d44a79ec96e1dc4e78
Heberth Junor <hjunor@gmail.com>
2021-02-21 19:42:59 -0300

    refactor: index server

--- src/index.js
+++ src/index.js
@@ -7,19 +7,25 @@ const checkJwt = require("./middlewares/jwt");
 
 const authController = require("./controllers/auth");
 const linkController = require("./controllers/link");
+const linksControler = require("./controllers/links");
+
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
 app.use(response);
-app.use(checkJwt);
+// app.use(checkJwt);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", authController);
 app.use("/link", linkController);
+app.use("/links", linksControler);
+
+
 
 db.sequelize.sync().then(() => {
-  app.listen(process.env.PORT || 3001, () => {
-    console.log("Server start 🔥");
+  app.listen(PORT, () => {
+    console.log("Server start 🔥", PORT);
   });
 });
