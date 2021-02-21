ddc2e56a48f041ea1412ab52f75984f0719e69ee
Heberth Junor <hjunor@gmail.com>
2021-02-21 19:43:38 -0300

    refactor: config

--- src/config/config.js
+++ src/config/config.js
@@ -2,22 +2,15 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    protocol: "postgres",
-    port: process.env.DB_PORT,
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
 
+  username: 'postgres',
+  password: 'senha',
+  database: 'post',
+  host: 'localhost',
+  dialect: "postgres",
+  protocol: "postgres",
+  port: 5432,
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
